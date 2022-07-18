75e0b46d05086e83b9c1291d14d1199ae3ae1cd2
KrasovskihIA <75630213+KrasovskihIA@users.noreply.github.com>
2022-07-18 14:23:26 +0400

    Creating a Journalistview

--- DiaryDjango/Journalapp/views.py
+++ DiaryDjango/Journalapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Journal
+from django.views.generic import ListView
 
-# Create your views here.
+class JournalListView(ListView):
+    model = Journal
+    template_name = "journal/journal_list.html"
+    context_object_name = "journal_list"
